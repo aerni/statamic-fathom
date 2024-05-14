c453a57405582fd8dc26fbdae47493356b50deed
Michael Aerni <hello@michaelaerni.ch>
2024-05-14 11:26:52 -0400

    Don’t autopublish config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,7 +4,6 @@ namespace Aerni\Fathom;
 
 use Statamic\Facades\CP\Nav;
 use Statamic\Providers\AddonServiceProvider;
-use Statamic\Statamic;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -14,29 +13,11 @@ class ServiceProvider extends AddonServiceProvider
 
     public function bootAddon(): void
     {
-        $this
-            ->createMenu()
-            ->autoPublishConfig();
-    }
-
-    protected function createMenu(): self
-    {
         Nav::extend(function ($nav) {
             $nav->create('Fathom')
                 ->section('Tools')
                 ->route('fathom')
                 ->icon('seo-search-graph');
         });
-
-        return $this;
-    }
-
-    protected function autoPublishConfig(): self
-    {
-        Statamic::afterInstalled(function ($command) {
-            $command->call('vendor:publish', ['--tag' => 'fathom-config']);
-        });
-
-        return $this;
     }
 }
